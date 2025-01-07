a1ef97b9bdada91a846578e3a225f4ea4db2aefb
DGLOliveira <dgloliveira90@gmail.com>
2025-01-07 14:07:26 -0100

    Update build for touchscreen testing on DrawDoc

--- build/static/js/242.755d75db.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpc=self.webpackChunkpc||[]).push([[242],{2242:(c,e,s)=>{s.r(e),s.d(e,{default:()=>p});const p=s(1462).acN}}]);
-//# sourceMappingURL=242.755d75db.chunk.js.map
