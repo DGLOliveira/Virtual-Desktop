7a618c03e778622bc1c394605a7d526b0e468377
DGLOliveira <dgloliveira90@gmail.com>
2025-01-10 18:51:14 -0100

    update build

---
+++ build/static/js/86.00715704.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpc=self.webpackChunkpc||[]).push([[86],{3086:(c,e,s)=>{s.r(e),s.d(e,{default:()=>p});const p=s(3677).UIo}}]);
+//# sourceMappingURL=86.00715704.chunk.js.map
