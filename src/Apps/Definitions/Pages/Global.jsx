510ac81d5a828bae2b9fa2cb815386be0e4e12bb
DGLOliveira <dgloliveira90@gmail.com>
2025-02-26 16:04:02 -0100

    Expanded Personalization font family list

--- src/Apps/Definitions/Pages/Global.jsx
+++ src/Apps/Definitions/Pages/Global.jsx
@@ -1,36 +1,66 @@
 import { useState, useEffect } from "react";
 
+const fontFamilyList = [
+    "Arial",
+    "Times New Roman",
+    "Monospace",
+    "Verdana",
+    "Georgia",
+    "Courier New",
+    "Tahoma",
+    "Garamond",
+    "Trebuchet MS",
+    "Impact",
+    "Helvetica",
+    "Arial Black",
+    "Comic Sans MS",
+    "Lucida Console",
+    "Lucida Sans Unicode",
+    "Lucida Sans",
+    "Palatino Linotype",
+    "Book Antiqua",
+    "Arial Narrow",
+    "Arial Rounded MT Bold",
+    "Brush Script MT",
+    "Century Gothic",
+    "Century Schoolbook"
+];
+
 export const Global = () => {
     const [fontFamily, setFontFamily] = useState(
         getComputedStyle(root).getPropertyValue("--GeneralFontFamily"));
     useEffect(() => {
         root.style.setProperty("--GeneralFontFamily", fontFamily);
     }, [fontFamily]);
-
+    const [theme, setTheme] = useState("Default");
     return (
         <>
-        <div>
-            <label>Font Family:</label>
-            <select
-                value={fontFamily}
-                onChange={(e) => setFontFamily(e.target.value)}
-            >
-                <option style={{ fontFamily: "Arial" }}>Arial</option>
-                <option style={{ fontFamily: "Times New Roman" }}>Times New Roman</option>
-                <option style={{ fontFamily: "Monospace" }}>Monospace</option>
-                <option style={{ fontFamily: "Verdana" }}>Verdana</option>
-                <option style={{ fontFamily: "Georgia" }}>Georgia</option>
-                <option style={{ fontFamily: "Courier New" }}>Courier New</option>
-                <option style={{ fontFamily: "Tahoma" }}>Tahoma</option>
-                <option style={{ fontFamily: "Garamond" }}>Garamond</option>
-                <option style={{ fontFamily: "Impact" }}>Impact</option>
-                <option style={{ fontFamily: "Helvetica" }}>Helvetica</option>
-                <option style={{ fontFamily: "Comic Sans MS" }}>Comic Sans MS</option>
-                <option style={{ fontFamily: "Lucida Console" }}>Lucida Console</option>
-                <option style={{ fontFamily: "Lucida Sans Unicode" }}>Lucida Sans Unicode</option>
-                <option style={{ fontFamily: "Palatino Linotype" }}>Palatino Linotype</option>
-            </select>
-            </div>
+            <fieldset>
+                <legend>Font Family:</legend>
+                <select
+                    value={fontFamily}
+                    onChange={(e) => setFontFamily(e.target.value)}
+                >
+                    {fontFamilyList.sort().map((font) => (
+                        <option key={font} style={{ fontFamily: font }}>
+                            {font}
+                        </option>
+                    ))}
+                </select>
+            </fieldset>
+            <fieldset>
+                <legend>Theme:</legend>
+                <select
+                    value={theme}
+                    onChange={(e) => setTheme(e.target.value)}
+                    disabled={true}
+                    title={"This feature is not available yet."}
+                >
+                    <option value="Default">Default</option>
+                    <option value="Aero">Aero</option>
+                    <option value="Aqua">Aqua</option>
+                </select>
+            </fieldset>
         </>
     )
 }
