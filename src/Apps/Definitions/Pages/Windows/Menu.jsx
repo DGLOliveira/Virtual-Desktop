403606ab9556e9dc4898ea305f0740e3d60f912c
DGLOliveira <dgloliveira90@gmail.com>
2025-03-17 22:58:08 -0100

    Expanded Window Menu customization

--- src/Apps/Definitions/Pages/Windows/Menu.jsx
+++ src/Apps/Definitions/Pages/Windows/Menu.jsx
@@ -140,26 +140,83 @@ export const Menu = () => {
         root.style.setProperty("--WindowMenuBorderType", borderType);
     }, [borderType]);
     //----------------------------------------------------------------------------//
-    const [hoverColor, setHoverColor] = useState(
-        getComputedStyle(root).getPropertyValue("--WindowMenuButtonHoverBkgr"),
+    const [hoverFontColor, setHoverFontColor] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuHoverColor"),
     );
     useEffect(() => {
-        root.style.setProperty("--WindowMenuButtonHoverBkgr", hoverColor);
-    }, [hoverColor]);
+        root.style.setProperty("--WindowMenuHoverColor", hoverFontColor);
+    }, [hoverFontColor]);
+    //----------------------------------------------------------------------------//
+    const [hoverBkgr, setHoverBkgr] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuHoverBkgr"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuHoverBkgr", hoverBkgr);
+    }, [hoverBkgr]);
+    //----------------------------------------------------------------------------//
+    const [navButtonBkgr, setNavButtonBkgr] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonBkgr"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonBkgr", navButtonBkgr);
+    }, [navButtonBkgr]);
+    //----------------------------------------------------------------------------//
+    const [navButtonFontColor, setNavButtonFontColor] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonColor"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonColor", navButtonFontColor);
+    }, [navButtonFontColor]);
+    //----------------------------------------------------------------------------//
+    const [navButtonHoverBkgr, setNavButtonHoverBkgr] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonHoverBkgr"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonHoverBkgr", navButtonHoverBkgr);
+    }, [navButtonHoverBkgr]);
+    //----------------------------------------------------------------------------//
+    const [navButtonHoverFontColor, setNavButtonHoverFontColor] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonHoverColor"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonHoverColor", navButtonHoverFontColor);
+    }, [navButtonHoverFontColor]);
+    //----------------------------------------------------------------------------//
+    const [navButtonBorderColor, setNavButtonBorderColor] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonBorderColor"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonBorderColor", navButtonBorderColor);
+    }, [navButtonBorderColor]);
+    //----------------------------------------------------------------------------//
+    const [navButtonBorderWidth, setNavButtonBorderWidth] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonBorderWidth").slice(0, -2),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonBorderWidth", navButtonBorderWidth + "px");
+    }), [navButtonBorderWidth];
+    //----------------------------------------------------------------------------//
+    const [navButtonBorderRadius, setNavButtonBorderRadius] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonBorderRadius").slice(0, -2),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonBorderRadius", navButtonBorderRadius + "px");
+    }, [navButtonBorderRadius]);
+    //----------------------------------------------------------------------------//
+    const [navButtonBorderType, setNavButtonBorderType] = useState(
+        getComputedStyle(root).getPropertyValue("--WindowMenuNavButtonBorderType"),
+    );
+    useEffect(() => {
+        root.style.setProperty("--WindowMenuNavButtonBorderType", navButtonBorderType);
+    }, [navButtonBorderType]);
+    //----------------------------------------------------------------------------//
+
 
 
     return (
         <>
             <fieldset>
                 <legend>Font</legend>
-                <div>
-                    <label>Font Color:</label>
-                    <ColorPicker
-                        color={fontColor}
-                        setColor={setFontColor}
-                        useAlpha={false}
-                    />
-                </div>
                 <div>
                     <label>Font Size:</label>
                     <input
@@ -173,7 +230,106 @@ export const Menu = () => {
                 </div>
             </fieldset>
             <fieldset>
-                <legend>Background</legend>
+                <legend>Navigation Buttons</legend>
+                <div>
+                    <label>Font Color:</label>
+                    <ColorPicker
+                        color={navButtonFontColor}
+                        setColor={setNavButtonFontColor}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <label>Hover Font Color:</label>
+                    <ColorPicker
+                        color={navButtonHoverFontColor}
+                        setColor={setNavButtonHoverFontColor}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <legend>Background</legend>
+                    <ColorPicker
+                        color={navButtonBkgr}
+                        setColor={setNavButtonBkgr}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <legend>Hover Background</legend>
+                    <ColorPicker
+                        color={navButtonHoverBkgr}
+                        setColor={setNavButtonHoverBkgr}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <label>Border Color:</label>
+                    <ColorPicker
+                        color={navButtonBorderColor}
+                        setColor={setNavButtonBorderColor}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <label>Border Width:</label>
+                    <input
+                        type="range"
+                        min={0}
+                        max={5}
+                        step="1"
+                        value={navButtonBorderWidth}
+                        onChange={(e) => setNavButtonBorderWidth(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Border Radius:</label>
+                    <input
+                        type="range"
+                        min={0}
+                        max={20}
+                        step="1"
+                        value={navButtonBorderRadius}
+                        onChange={(e) => setNavButtonBorderRadius(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Border Type:</label>
+                    <select
+                        value={navButtonBorderType}
+                        onChange={(e) => setNavButtonBorderType(e.target.value)}
+                    >
+                        <option value="solid">Solid</option>
+                        <option value="dotted">Dotted</option>
+                        <option value="dashed">Dashed</option>
+                        <option value="double">Double</option>
+                        <option value="groove">Groove</option>
+                        <option value="ridge">Ridge</option>
+                        <option value="inset">Inset</option>
+                        <option value="outset">Outset</option>
+                    </select>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>Menu</legend>
+
+                <div>
+                    <label>Font Color:</label>
+                    <ColorPicker
+                        color={fontColor}
+                        setColor={setFontColor}
+                        useAlpha={false}
+                    />
+                </div>
+                <div>
+                    <label>Hover Font Color:</label>
+                    <ColorPicker
+                        color={hoverFontColor}
+                        setColor={setHoverFontColor}
+                        useAlpha={false}
+                    />
+                </div>
                 <div>
                     <label>Background Color:</label>
                     <ColorPicker
@@ -182,9 +338,14 @@ export const Menu = () => {
                         useAlpha={true}
                     />
                 </div>
-            </fieldset>
-            <fieldset>
-                <legend>Border</legend>
+                <div>
+                    <label>Hover Background:</label>
+                    <ColorPicker
+                        color={hoverBkgr}
+                        setColor={setHoverBkgr}
+                        useAlpha={true}
+                    />
+                </div>
                 <div>
                     <label>Border Color:</label>
                     <ColorPicker
@@ -227,17 +388,6 @@ export const Menu = () => {
                         <option value="inset">inset</option>
                         <option value="outset">outset</option>
                     </select>
-                </div>
-            </fieldset>
-            <fieldset>
-                <legend>Hover</legend>
-                <div>
-                    <label>Hover Color:</label>
-                    <ColorPicker
-                        color={hoverColor}
-                        setColor={setHoverColor}
-                        useAlpha={true}
-                    />
                 </div>
             </fieldset>
         </>
