07a82b59726cc55cab9ba4781345aeadb0a3ce6c
DGLOliveira <dgloliveira90@gmail.com>
2025-05-08 13:38:30 +0000

    Added window resizing logic for touchscreen

--- src/System/AppManager/Components/AppResizer.jsx
+++ src/System/AppManager/Components/AppResizer.jsx
@@ -8,39 +8,46 @@ export const AppResizer = ({ appName }) => {
     const [startPos, setStartPos] = useState({ x: 0, y: 0 });
     const [startSize, setStartSize] = useState({ x: 0, y: 0 });
 
-    const handleMouseResizeStart = (e) => {
-        setStartPos({ x: e.clientX, y: e.clientY });
+    const resizeStart = (e) => {
+        if (e.type === "touchstart") {
+            setStartPos({ 
+                x: e.touches[0].clientX, 
+                y: e.touches[0].clientY 
+            });
+        } else {
+            setStartPos({ 
+                x: e.clientX, 
+                y: e.clientY 
+            });
+        }
         setStartSize({
             x: appContext.apps[appName].Size.Current.width,
             y: appContext.apps[appName].Size.Current.height
         });
     };
 
-    const handleMouseResize = (e, direction) => {
+    const resize = (e, direction) => {
         e.preventDefault();
+        let clientX, clientY;
+        if (e.type === "touchstart") {
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
+        } else {
+            clientX = e.clientX;
+            clientY = e.clientY;
+        }
         if (e.clientX !== 0 && e.clientY !== 0) {
-            let x = startPos.x - e.clientX;
-            let y = startPos.y - e.clientY;
-            appContext.setResize(appName, direction, x, y, startSize.y, startSize.x);
+        let x = startPos.x - clientX;
+        let y = startPos.y - clientY;
+        appContext.setResize(appName, direction, x, y, startSize.y, startSize.x);
         }
     };
 
-    const handleMouseResizeEnd = (e, direction) => {
+    const resizeEnd = (e, direction) => {
         e.preventDefault();
-        handleMouseResize(e, direction);
+        resize(e, direction);
         setStartPos({ x: 0, y: 0 });
-        setStartSize({ x:0, y:0 });
-    };
-
-    const handleTouchResizeStart = (e) => {
-        e.preventDefault();
-        setStartPos({ x: e.touches[0].clientX, y: e.touches[0].clientY });
-        console.log(e.touches[0].clientX, e.touches[0].clientY);
-    };
-
-    const handleTouchResize = (e, direction) => {
-        e.preventDefault();
-        console.log(e);
+        setStartSize({ x: 0, y: 0 });
     };
 
     return <>
@@ -49,67 +56,67 @@ export const AppResizer = ({ appName }) => {
             <>
                 <app-resize-n
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "n")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "n")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "n")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "n")}
+                    onDragEnd={(e) => resizeEnd(e, "n")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "n")}
                 />
                 <app-resize-e
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "e")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "e")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "e")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "e")}
+                    onDragEnd={(e) => resizeEnd(e, "e")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "e")}
                 />
                 <app-resize-s
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "s")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "s")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "s")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "s")}
+                    onDragEnd={(e) => resizeEnd(e, "s")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "s")}
                 />
                 <app-resize-w
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "w")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "w")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "w")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "w")}
+                    onDragEnd={(e) => resizeEnd(e, "w")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "w")}
                 />
                 <app-resize-ne
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "ne")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "ne")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "ne")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "ne")}
+                    onDragEnd={(e) => resizeEnd(e, "ne")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "ne")}
                 />
                 <app-resize-nw
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "nw")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "nw")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "nw")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "nw")}
+                    onDragEnd={(e) => resizeEnd(e, "nw")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "nw")}
                 />
                 <app-resize-sw
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "sw")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "sw")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "sw")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "sw")}
+                    onDragEnd={(e) => resizeEnd(e, "sw")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "sw")}
                 />
                 <app-resize-se
                     draggable={true}
-                    onDragStart={(e) => handleMouseResizeStart(e)}
-                    onDrag={(e) => handleMouseResize(e, "se")}
-                    onDragEnd={(e) => handleMouseResizeEnd(e, "se")}
-                    onTouchStart={(e) => handleTouchResizeStart(e)}
-                    onTouchMove={(e) => handleTouchResize(e, "se")}
+                    onDragStart={(e) => resizeStart(e)}
+                    onDrag={(e) => resize(e, "se")}
+                    onDragEnd={(e) => resizeEnd(e, "se")}
+                    onTouchStart={(e) => resizeStart(e)}
+                    onTouchMove={(e) => resize(e, "se")}
                 />
             </>}
     </>;
