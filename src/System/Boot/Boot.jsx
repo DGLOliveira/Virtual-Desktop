52a3b6c344a5c5fe252333589a9dc7de57102cbf
DGLOliveira <dgloliveira90@gmail.com>
2025-05-14 17:12:25 +0000

    Expanded boot page with more device info

--- src/System/Boot/Boot.jsx
+++ src/System/Boot/Boot.jsx
@@ -27,10 +27,17 @@ export default function Boot() {
       const listItem = document.createElement("li");
       let text = "";
       if(typeof info[key] === "object") {
+        let flag = false;
         text = key + ": ";
         Object.keys(info[key]).forEach((subkey) => {
+          if(info[key][subkey] !== "Unknown"){
           text += info[key][subkey] + " ";
+            flag = true;
+          };
         });
+        if(!flag){
+          text += "Unknown";
+        }
       }else{
         text = key + ": " + info[key];
       }
@@ -49,13 +56,22 @@ export default function Boot() {
   }
 
   function checkDevice() {
+    //There is no follproof way to check for touch support across all devices
+    //Touch interface should always be included when needed
     const touch = (('ontouchstart' in window) ||
       (navigator.maxTouchPoints > 0) ||
       (navigator.msMaxTouchPoints > 0)) ? "Yes" : "No";
-
+    //RAM values currently not available in Firefox and Safari
+    //https://developer.mozilla.org/en-US/docs/Web/API/Device_Memory_API
+    const ram = navigator.deviceMemory ? navigator.deviceMemory + " GB" : "Unknown";
     deviceInfo = {
-      "Device": checkFalsy(uap.getDevice().type),
+      "Device": {
+        name: checkFalsy(uap.getDevice().model),
+        vendor: checkFalsy(uap.getDevice().vendor),
+      },
+      "Type": checkFalsy(uap.getDevice().type),
       "CPU Architecture": checkFalsy(uap.getCPU().architecture),
+      "RAM": ram,
       "OS": {
         name: checkFalsy(uap.getOS().name),
         version: checkFalsy(uap.getOS().version)
@@ -67,15 +83,62 @@ export default function Boot() {
 
   function checkBrowser() {
     const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "Yes" : "No";
+    const cookiesEnabled = navigator.cookieEnabled ? "Yes" : "No";
     browserInfo = {
       "Browser": {
         name: checkFalsy(uap.getBrowser().name),
         version: checkFalsy(uap.getBrowser().version)
       },
-      "Dark Mode": darkMode
+      "Dark Mode": darkMode,
+      "Cookies Enabled": cookiesEnabled,
     }
     return browserInfo;
   }
+
+  //-----------------------------------------------------------//
+  //-----------------------------------------------------------//
+  //-----------------------------------------------------------//
+  // Functions bellow under construction and/or for testing
+
+  console.log(navigator);
+  console.log(uap.getResult());
+
+  //Currently not available in Firefox and Safari
+  //https://developer.mozilla.org/en-US/docs/Web/API/BatteryManager
+  if(navigator.getBattery){
+    navigator.getBattery().then((battery) => {
+      console.log(battery);
+    });
+  }
+
+  //No current or future use expected
+  if(navigator.mediaDevices){
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      console.log(devices);
+    });
+  }
+
+  //No current or future use expected
+  //Currently experimental feature, only parcially available in Chrome
+  //https://developer.mozilla.org/en-US/docs/Web/API/Navigator/bluetooth
+  if(navigator.bluetooth){
+    navigator.bluetooth.getAvailability().then((availability) => {
+      console.log("bluetooth: " + availability);
+    })
+  }
+
+  //No current or future use expected
+  //Currently experimental feature, compatibility table unknown
+  //https://developer.mozilla.org/en-US/docs/Web/API/WebUSB_API
+  if(navigator.usb){
+    navigator.usb.getDevices().then((devices) => {
+      console.log(devices);
+    });
+  }
+
+  //-----------------------------------------------------------//
+  //-----------------------------------------------------------//
+  //-----------------------------------------------------------//
 
   useEffect(() => {
     createList("Device", checkDevice());
