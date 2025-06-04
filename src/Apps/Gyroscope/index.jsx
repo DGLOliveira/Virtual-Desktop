2bc402848d7722598bfc9e2443722897958d91d8
DGLOliveira <dgloliveira90@gmail.com>
2025-06-04 17:32:34 +0000

    Changed default angles for landscape orientation

--- src/Apps/Gyroscope/index.jsx
+++ src/Apps/Gyroscope/index.jsx
@@ -38,11 +38,11 @@ export default function Gyroscope() {
                 let gama = accDelta;
                 console.log(angles);
                 if (angles.screenOrientationAngle === 90) {
-                    alpha = angles.roll + Math.PI / 2;
-                    beta = -angles.pitch + Math.PI;
+                    alpha = angles.roll + Math.PI;
+                    beta = -angles.pitch + Math.PI/2;
                 } else if (angles.screenOrientationAngle === 270) {
-                    alpha = -angles.roll - Math.PI / 2;
-                    beta = -angles.pitch + Math.PI;
+                    alpha = -angles.roll - Math.PI;
+                    beta = -angles.pitch + Math.PI/2;
                 } else if (angles.screenOrientationAngle === 0) {
                     alpha = angles.pitch + Math.PI;
                     beta = angles.roll + Math.PI / 2;
