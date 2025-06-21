62d0f71cd5a12fc3bc5ed91f3bc76c4bf6d00361
DGLOliveira <dgloliveira90@gmail.com>
2025-06-21 17:22:46 +0000

    Fixed minor mistake in aero theme live app button

--- src/System/ThemeManager/Themes/Aero/LiveAppButton/content.jsx
+++ src/System/ThemeManager/Themes/Aero/LiveAppButton/content.jsx
@@ -5,7 +5,7 @@ export default function LiveAppButton({ name, click, context, AppIcon, isSelecte
         <button
             onClick={click}
             onContextMenu={(e) => {context(e)}}
-            className={isSelected ? "liveAppsAero liveAppsAeroActive" : "liveAppsAeroAqua"}
+            className={isSelected ? "liveAppsAero liveAppsAeroActive" : "liveAppsAero"}
             aria-label={"Live App" + name}
         >
             <AppIcon appName={name} />
