b430ba5895fdb5222cb3b46da700c96c2aedff30
DGLOliveira <dgloliveira90@gmail.com>
2025-06-25 19:12:14 +0000

    Expanded taskbar mobile tray to expand/contract on drag

--- src/System/Taskbar/Components/Tray/Tray.jsx
+++ src/System/Taskbar/Components/Tray/Tray.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { createPortal } from "react-dom";
 
 import { TaskbarClock } from "../../Components/Clock/index.jsx";
@@ -10,6 +10,50 @@ import "./styles.css";
 
 export const Tray = ({ showWeather, setShowWeather, showClock, setShowClock, contextMenu }) => {
     const device = useContext(DeviceContext);
+    const [dragY, setDragY] = useState({ start: 0, current: 0 });
+    const [availableHeight, setAvailableHeight] = useState(document.getElementsByTagName("desk-top")[0].clientHeight);
+    const [heightOffset, setHeightOffset] = useState(0);
+
+    const handleDragStart = (e) => {
+        setAvailableHeight(document.getElementsByTagName("desk-top")[0].clientHeight);
+        setHeightOffset(document.getElementsByTagName("mobile-tray")[0].clientHeight);
+        if (e.type === "touchstart") {
+            setDragY({ start: e.touches[0].clientY, current: 0 })
+        }
+        else {
+            setDragY({ start: e.clientY, current: 0 })
+        }
+    };
+    const handleDrag = (e) => {
+        e.preventDefault();
+        if (e.type.includes("drag")) {
+            setDragY({ ...dragY, current: e.clientY });
+        } else {
+            setDragY({ ...dragY, current: e.touches[0].clientY });
+        }
+    };
+    const handleDragEnd = (e) => {
+        let endY;
+        if (e.type.includes("drag")) {
+            endY = e.clientY;
+        } else {
+            endY = e.touches[0].clientY;
+        }
+        if (device.virtualOSState.display === "tray") {
+            if (window.innerHeight * 0.2 < dragY.start - endY) {
+                device.setVirtualOSState({ ...device.virtualOSState, display: "none" })
+            }
+        } else {
+            if (window.innerHeight * 0.2 < endY - dragY.start) {
+                device.setVirtualOSState({ ...device.virtualOSState, display: "tray" })
+            }
+        }
+        setDragY({start: 0, current: 0});
+    };
+
+
+
+
     return (<>
         {device.deviceType === "Desktop" ?
             <><taskbar-tray>
@@ -20,12 +64,37 @@ export const Tray = ({ showWeather, setShowWeather, showClock, setShowClock, con
                 <vertical-rect />
             </>
             : createPortal(
-                <mobile-tray>
-                    {showWeather && <Weather contextMenu={contextMenu} setShowWeather={setShowWeather} />}
-                    <DeviceSet />
-                    {showClock && <TaskbarClock contextMenu={contextMenu} setShowClock={setShowClock} />}
-                </mobile-tray>
-            , document.getElementById("root"))
+                <>
+                    <mobile-tray
+                        draggable="true"
+                        onDragStart={(e) => handleDragStart(e)}
+                        onDrag={(e) => handleDrag(e)}
+                        onDragEnd={(e) => handleDragEnd(e)}
+                    >
+                        {showWeather && <Weather contextMenu={contextMenu} setShowWeather={setShowWeather} />}
+                        <DeviceSet />
+                        {showClock && <TaskbarClock contextMenu={contextMenu} setShowClock={setShowClock} />}
+                    </mobile-tray>
+                    {createPortal(
+                        <mobile-tray-fullscreen
+                            style={{
+                                top: dragY.current - dragY.start !== 0 ? 
+                                device.virtualOSState.display === "tray" 
+                                    ? (dragY.current - dragY.start > 0 ? "0": `${-heightOffset + dragY.current - dragY.start}px` ) 
+                                    : (dragY.current - dragY.start < 0 ? "-100%" : `${-heightOffset-availableHeight + dragY.current - dragY.start}px` ) 
+                                : device.virtualOSState.display === "tray" ? "0" : "-100%",
+                            }}
+                            draggable="true"
+                            onDragStart={(e) => handleDragStart(e)}
+                            onDrag={(e) => handleDrag(e)}
+                            onDragEnd={(e) => handleDragEnd(e)}
+                        >
+
+                        </mobile-tray-fullscreen>
+                        , document.getElementsByTagName("desk-top")[0])
+                    }
+                </>
+                , document.getElementById("root"))
         }
     </>
     );
