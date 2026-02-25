45040ef3ca53294f21ad369f5e39fd538be28466
DGLOliveira <dgloliveira90@gmail.com>
2026-02-25 18:24:13 -0100

    Added "notranslate" class to Calculator app buttons to prevent odd translations and styling problems;

--- src/Apps/Calculator/Components/Keypad.jsx
+++ src/Apps/Calculator/Components/Keypad.jsx
@@ -14,15 +14,15 @@ export default function Keypad({ handleInput, calculatorState, setCalculatorStat
                         <div
                             className={
                                 calculatorState.alt
-                                    ? "calculatorButton calculatorTypeButtonSelected"
-                                    : "calculatorButton"
+                                    ? "calculatorButton calculatorTypeButtonSelected notranslate"
+                                    : "calculatorButton notranslate"
                             }
                             onClick={() => setCalculatorState({ ...calculatorState, alt: !calculatorState.alt, })}
                         >
                             Alt
                         </div>
                         <div
-                            className={calculatorState.trig ? "calculatorButton calculatorTypeButtonSelected" : "calculatorButton"}
+                            className={calculatorState.trig ? "calculatorButton calculatorTypeButtonSelected notranslate" : "calculatorButton notranslate"}
                             onClick={() => setCalculatorState({ ...calculatorState, trig: !calculatorState.trig, })}
                         >
                             Trig
@@ -32,22 +32,22 @@ export default function Keypad({ handleInput, calculatorState, setCalculatorStat
                     <></>
                 )}
                 {calculatorState.type !== "Graphic" ? (
-                    <div style={{ background: "royalblue" }} className="calculatorButton" onClick={() => handleInput("Ans")}>Ans</div>
+                    <div style={{ background: "royalblue" }} className="calculatorButton notranslate" onClick={() => handleInput("Ans")}>Ans</div>
                 ) : (
-                    <div style={{ background: "royalblue" }} className="calculatorButton" onClick={() => handleInput("𝑥")}>𝑥</div>
+                    <div style={{ background: "royalblue" }} className="calculatorButton notranslate" onClick={() => handleInput("𝑥")}>𝑥</div>
                 )}
-                <div style={{ background: "red" }} className="calculatorButton" onClick={() => handleInput("C")}>C</div>
-                <div style={{ background: "darkorange" }} className="calculatorButton" onClick={() => handleInput("←")}>←</div>
+                <div style={{ background: "red" }} className="calculatorButton notranslate" onClick={() => handleInput("C")}>C</div>
+                <div style={{ background: "darkorange" }} className="calculatorButton notranslate" onClick={() => handleInput("←")}>←</div>
                 {calculatorState.type !== "Graphic" ? (
-                    <div style={{ background: "green" }} className="calculatorButton" onClick={() => handleInput("=")}>=</div>
+                    <div style={{ background: "green" }} className="calculatorButton notranslate" onClick={() => handleInput("=")}>=</div>
                 ) : (
                     <>
-                        <div style={{ background: "green" }} className="calculatorButton" onClick={() => handleInput("=")}>Draw</div>
+                        <div style={{ background: "green" }} className="calculatorButton notranslate" onClick={() => handleInput("=")}>Draw</div>
                         <div
                             className={
                                 calculatorState.window
-                                    ? "calculatorButton"
-                                    : "calculatorButton calculatorButtonSmall"
+                                    ? "calculatorButton notranslate"
+                                    : "calculatorButton calculatorButtonSmall notranslate"
                             }
                             onClick={() =>
                                 setCalculatorState({
@@ -71,23 +71,23 @@ export default function Keypad({ handleInput, calculatorState, setCalculatorStat
                     }
                 >
                     <div className="calculatorNumpadRow">
-                        <div className="calculatorButton" onClick={() => handleInput(7)}>7</div>
-                        <div className="calculatorButton" onClick={() => handleInput(8)}>8</div>
-                        <div className="calculatorButton" onClick={() => handleInput(9)}>9</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(7)}>7</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(8)}>8</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(9)}>9</div>
                     </div>
                     <div className="calculatorNumpadRow">
-                        <div className="calculatorButton" onClick={() => handleInput(4)}>4</div>
-                        <div className="calculatorButton" onClick={() => handleInput(5)}>5</div>
-                        <div className="calculatorButton" onClick={() => handleInput(6)}>6</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(4)}>4</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(5)}>5</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(6)}>6</div>
                     </div>
                     <div className="calculatorNumpadRow">
-                        <div className="calculatorButton" onClick={() => handleInput(1)}>1</div>
-                        <div className="calculatorButton" onClick={() => handleInput(2)}>2</div>
-                        <div className="calculatorButton" onClick={() => handleInput(3)}>3</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(1)}>1</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(2)}>2</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(3)}>3</div>
                     </div>
                     <div className="calculatorNumpadRow">
-                        <div className="calculatorButton" onClick={() => handleInput(0)} style={{ flexGrow: "2" }}>0</div>
-                        <div className="calculatorButton" onClick={() => handleInput(".")} style={{ flexGrow: "1" }}>.</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(0)} style={{ flexGrow: "2" }}>0</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput(".")} style={{ flexGrow: "1" }}>.</div>
                     </div>
                 </div>
                 <div
@@ -99,37 +99,37 @@ export default function Keypad({ handleInput, calculatorState, setCalculatorStat
                     }
                 >
                     <div className="calculatorExpressionsColumn">
-                        <div className="calculatorButton" onClick={() => handleInput("+")}>+</div>
-                        <div className="calculatorButton" onClick={() => handleInput("-")}>-</div>
-                        <div className="calculatorButton" onClick={() => handleInput("×")}>×</div>
-                        <div className="calculatorButton" onClick={() => handleInput("÷")}>÷</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput("+")}>+</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput("-")}>-</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput("×")}>×</div>
+                        <div className="calculatorButton notranslate" onClick={() => handleInput("÷")}>÷</div>
                     </div>
                     {calculatorState.type !== "Basic" ? (
                         <>
                             <div className="calculatorExpressionsColumn">
                                 {!calculatorState.alt ? (
-                                    <div className="calculatorButton" onClick={() => handleInput("²")}>𝑥²</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("²")}>𝑥²</div>
                                 ) : (
-                                    <div className="calculatorButton" onClick={() => handleInput("³")}>𝑥³</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("³")}>𝑥³</div>
                                 )}
                                 {!calculatorState.alt ? (
-                                    <div className="calculatorButton" onClick={() => handleInput("√²")}>√²</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("√²")}>√²</div>
                                 ) : (
-                                    <div className="calculatorButton" onClick={() => handleInput("√³")}>√³</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("√³")}>√³</div>
                                 )}
-                                <div className="calculatorButton" onClick={() => handleInput("^")}>
+                                <div className="calculatorButton notranslate" onClick={() => handleInput("^")}>
                                     <div>
                                         𝑦<sup>𝑥</sup>
                                     </div>
                                 </div>
                                 {!calculatorState.alt ? (
-                                    <div className="calculatorButton" onClick={() => handleInput("log₁₀")}>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("log₁₀")}>
                                         <div>
                                             log<sub>₁₀</sub>
                                         </div>
                                     </div>
                                 ) : (
-                                    <div className="calculatorButton" onClick={() => handleInput("logₑ")}>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("logₑ")}>
                                         <div>
                                             log<sub>ₑ</sub>
                                         </div>
@@ -140,45 +140,45 @@ export default function Keypad({ handleInput, calculatorState, setCalculatorStat
                                 {!calculatorState.alt ? (
                                     !calculatorState.trig ? (
                                         <>
-                                            <div className="calculatorButton" onClick={() => handleInput("sin")}>sin</div>
-                                            <div className="calculatorButton" onClick={() => handleInput("cos")}>cos</div>
-                                            <div className="calculatorButton" onClick={() => handleInput("tan")}>tan</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("sin")}>sin</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("cos")}>cos</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("tan")}>tan</div>
                                         </>
                                     ) : (
                                         <>
-                                            <div className="calculatorButton" onClick={() => handleInput("sinh")}>sinh</div>
-                                            <div className="calculatorButton" onClick={() => handleInput("cosh")}>cosh</div>
-                                            <div className="calculatorButton" onClick={() => handleInput("tanh")}>tanh</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("sinh")}>sinh</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("cosh")}>cosh</div>
+                                            <div className="calculatorButton notranslate" onClick={() => handleInput("tanh")}>tanh</div>
                                         </>
                                     )
                                 ) : !calculatorState.trig ? (
                                     <>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("sin⁻¹")}>sin⁻¹</div>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("cos⁻¹")}>cos⁻¹</div>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("tan⁻¹")}>tan⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("sin⁻¹")}>sin⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("cos⁻¹")}>cos⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("tan⁻¹")}>tan⁻¹</div>
                                     </>
                                 ) : (
                                     <>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("sinh⁻¹")}>sinh⁻¹</div>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("cosh⁻¹")}>cosh⁻¹</div>
-                                        <div className="calculatorButton calculatorButtonSmall" onClick={() => handleInput("tanh⁻¹")}>tanh⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("sinh⁻¹")}>sinh⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("cosh⁻¹")}>cosh⁻¹</div>
+                                        <div className="calculatorButton calculatorButtonSmall notranslate" onClick={() => handleInput("tanh⁻¹")}>tanh⁻¹</div>
                                     </>
                                 )}
                                 {!calculatorState.alt ? (
-                                    <div className="calculatorButton" onClick={() => handleInput("°")}>°</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("°")}>°</div>
                                 ) : (
-                                    <div className="calculatorButton" onClick={() => handleInput("rad")}>rad</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("rad")}>rad</div>
                                 )}
                             </div>
                             <div className="calculatorExpressionsColumn">
                                 {calculatorState.alt ? (
-                                    <div className="calculatorButton" onClick={() => handleInput(")")}>)</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput(")")}>)</div>
                                 ) : (
-                                    <div className="calculatorButton" onClick={() => handleInput("(")}>(</div>
+                                    <div className="calculatorButton notranslate" onClick={() => handleInput("(")}>(</div>
                                 )}
-                                <div className="calculatorButton" onClick={() => handleInput("!")}>𝑥!</div>
-                                <div className="calculatorButton" onClick={() => handleInput("π")}>π</div>
-                                <div className="calculatorButton" onClick={() => handleInput("𝑒")}>𝑒</div>
+                                <div className="calculatorButton notranslate" onClick={() => handleInput("!")}>𝑥!</div>
+                                <div className="calculatorButton notranslate" onClick={() => handleInput("π")}>π</div>
+                                <div className="calculatorButton notranslate" onClick={() => handleInput("𝑒")}>𝑒</div>
                             </div>
                         </>
                     ) : (
