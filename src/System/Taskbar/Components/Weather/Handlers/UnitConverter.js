4ff8ea3a23ff87f9cfaf51c69221fe361bf46315
DGLOliveira <dgloliveira90@gmail.com>
2026-02-26 12:51:03 -0100

    Fixed issue with Weather unit conversion where values were being interpreted as strings instead of numbers

--- src/System/Taskbar/Components/Weather/Handlers/UnitConverter.js
+++ src/System/Taskbar/Components/Weather/Handlers/UnitConverter.js
@@ -1,33 +1,34 @@
 export const unitConverter = (type, source, target, value) => {
+    let val = Number(value);
     switch (type) {
         case "Temperature":
             switch (source) {
                 case "°C":
                     switch (target) {
                         case "°C":
-                            return value;
+                            return val;
                         case "°F":
-                            return ((value * 9 / 5) + 32).toFixed(1);
+                            return ((val * 9 / 5) + 32).toFixed(1);
                         case "K":
-                            return (value + 273.15).toFixed(1);
+                            return (val + 273.15).toFixed(1);
                     }
                 case "°F":
                     switch (target) {
                         case "°C":
-                            return ((value - 32) * 5 / 9).toFixed(1);
+                            return ((val - 32) * 5 / 9).toFixed(1);
                         case "°F":
-                            return value;
+                            return val;
                         case "K":
-                            return (((value - 32) * 5 / 9 + 273.15)).toFixed(1);
+                            return ((val - 32) * 5 / 9 + 273.15).toFixed(1);
                     }
                 case "K":
                     switch (target) {
                         case "°C":
-                            return (value - 273.15).toFixed(1);
+                            return (val - 273.15).toFixed(1);
                         case "°F":
-                            return (((value - 273.15) * 9 / 5 + 32)).toFixed(1);
+                            return ((val - 273.15) * 9 / 5 + 32).toFixed(1);
                         case "K":
-                            return value;
+                            return val;
                     }
             }
         case "Precipitation":
@@ -35,16 +36,16 @@ export const unitConverter = (type, source, target, value) => {
                 case "mm":
                     switch (target) {
                         case "mm":
-                            return value;
+                            return val;
                         case "in":
-                            return (value / 25.4).toFixed(3);
+                            return (val / 25.4).toFixed(3);
                     }
                 case "in":
                     switch (target) {
                         case "mm":
-                            return (value * 25.4).toFixed(1);
+                            return (val * 25.4).toFixed(1);
                         case "in":
-                            return value;
+                            return val;
                     }
             }
         case "Wind":
@@ -52,67 +53,67 @@ export const unitConverter = (type, source, target, value) => {
                 case "km/h":
                     switch (target) {
                         case "km/h":
-                            return value;
+                            return val;
                         case "mph":
-                            return (value / 1.60934).toFixed(1);
+                            return (val / 1.60934).toFixed(1);
                         case "m/s":
-                            return (value / 3.6).toFixed(1);
+                            return (val / 3.6).toFixed(1);
                         case "ft/s":
-                            return (value / 0.44704).toFixed(1);
+                            return (val / 0.44704).toFixed(1);
                         case "knots":
-                            return (value / 1.852).toFixed(1);
+                            return (val / 1.852).toFixed(1);
                     }
                 case "mph":
                     switch (target) {
                         case "km/h":
-                            return (value * 1.60934).toFixed(1);
+                            return (val * 1.60934).toFixed(1);
                         case "mph":
-                            return value;
+                            return val;
                         case "m/s":
-                            return (value * 0.44704).toFixed(1);
+                            return (val * 0.44704).toFixed(1);
                         case "ft/s":
-                            return (value * 0.868976).toFixed(1);
+                            return (val * 0.868976).toFixed(1);
                         case "knots":
-                            return (value / 1.15078).toFixed(1);
+                            return (val / 1.15078).toFixed(1);
                     }
                 case "m/s":
                     switch (target) {
                         case "km/h":
-                            return (value * 3.6).toFixed(1);
+                            return (val * 3.6).toFixed(1);
                         case "mph":
-                            return (value * 2.23694).toFixed(1);
+                            return (val * 2.23694).toFixed(1);
                         case "m/s":
-                            return value;
+                            return val;
                         case "ft/s":
-                            return (value * 3.28084).toFixed(1);
+                            return (val * 3.28084).toFixed(1);
                         case "knots":
-                            return (value / 0.51444).toFixed(1);
+                            return (val / 0.51444).toFixed(1);
                     }
                 case "ft/s":
                     switch (target) {
                         case "km/h":
-                            return (value * 0.44704).toFixed(1);
+                            return (val * 0.44704).toFixed(1);
                         case "mph":
-                            return (value * 0.868976).toFixed(1);
+                            return (val * 0.868976).toFixed(1);
                         case "m/s":
-                            return (value * 0.3048).toFixed(1);
+                            return (val * 0.3048).toFixed(1);
                         case "ft/s":
-                            return value;
+                            return val;
                         case "knots":
-                            return (value / 1.68781).toFixed(1);
+                            return (val / 1.68781).toFixed(1);
                     }
                 case "knots":
                     switch (target) {
                         case "km/h":
-                            return (value * 1.852).toFixed(1);
+                            return (val * 1.852).toFixed(1);
                         case "mph":
-                            return (value * 1.15078).toFixed(1);
+                            return (val * 1.15078).toFixed(1);
                         case "m/s":
-                            return (value * 0.51444).toFixed(1);
+                            return (val * 0.51444).toFixed(1);
                         case "ft/s":
-                            return (value * 1.68781).toFixed(1);
+                            return (val * 1.68781).toFixed(1);
                         case "knots":
-                            return value;
+                            return val;
                     }
             }
         case "Pressure":
@@ -120,70 +121,70 @@ export const unitConverter = (type, source, target, value) => {
                 case "hPa":
                     switch (target) {
                         case "hPa":
-                            return value;
+                            return val;
                         case "psi":
-                            return value / 68.9475729;
+                            return val / 68.9475729;
                         case "mmHg":
-                            return value / 133.322368;
+                            return val / 133.322368;
                         case "inHg":
-                            return value / 3386.38;
+                            return val / 3386.38;
                         case "atm":
-                            return value / 1013.25;
+                            return val / 1013.25;
                     }
                 case "psi":
                     switch (target) {
                         case "hPa":
-                            return value * 68.9475729;
+                            return val * 68.9475729;
                         case "psi":
-                            return value;
+                            return val;
                         case "mmHg":
-                            return value * 6.89475729;
+                            return val * 6.89475729;
                         case "inHg":
-                            return value * 0.0689475729;
+                            return val * 0.0689475729;
                         case "atm":
-                            return value / 14.6959487751941;
+                            return val / 14.6959487751941;
                     }
                 case "mmHg":
                     switch (target) {
                         case "hPa":
-                            return value * 133.322368;
+                            return val * 133.322368;
                         case "psi":
-                            return value * 0.0680459637;
+                            return val * 0.0680459637;
                         case "mmHg":
-                            return value;
+                            return val;
                         case "inHg":
-                            return value / 25.4;
+                            return val / 25.4;
                         case "atm":
-                            return value / 760;
+                            return val / 760;
                     }
                 case "inHg":
                     switch (target) {
                         case "hPa":
-                            return value * 3386.38;
+                            return val * 3386.38;
                         case "psi":
-                            return value * 0.491154;
+                            return val * 0.491154;
                         case "mmHg":
-                            return value * 25.4;
+                            return val * 25.4;
                         case "inHg":
-                            return value;
+                            return val;
                         case "atm":
-                            return value / 29.92126;
+                            return val / 29.92126;
                     }
                 case "atm":
                     switch (target) {
                         case "hPa":
-                            return value * 1013.25;
+                            return val * 1013.25;
                         case "psi":
-                            return value * 14.6959487751941;
+                            return val * 14.6959487751941;
                         case "mmHg":
-                            return value * 760;
+                            return val * 760;
                         case "inHg":
-                            return value * 29.92126;
+                            return val * 29.92126;
                         case "atm":
-                            return value;
+                            return val;
                     }
             }
         default:
-            return value;
+            return val;
     }
 }
